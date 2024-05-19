f15338647c87085a8f28d633605f81a94d3a167e
Squidknee <72271842+Squidknee@users.noreply.github.com>
2024-05-19 16:43:08 -0400

    orientation fixed and movement added

--- Week1-Project/Assets/_app/Scripts/Player/CharacterController.cs
+++ Week1-Project/Assets/_app/Scripts/Player/CharacterController.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEditor.Searcher;
 using UnityEngine;
 
 namespace _app.Scripts.Player
@@ -9,7 +10,12 @@ namespace _app.Scripts.Player
         public float movementSpeed;
         public Vector3 jumpForce;
         public bool crouching;
-
+        
+        public Transform orientation;
+        private float horizontalInput;
+        private float verticalInput;
+        public Vector3 moveDirection;
+        
         [Header("Player Components")] 
         public Rigidbody rb;
         public Camera playerCamera;
@@ -17,9 +23,18 @@ namespace _app.Scripts.Player
         private void Start()
         {
             rb = GetComponent<Rigidbody>();
+            rb.freezeRotation = true;
         }
+        
         public void Update()
         {
+            HandleInput();
+        }
+        private void HandleInput()
+        {
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+            verticalInput = Input.GetAxisRaw("Vertical");
+
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 PlayerJump();
@@ -31,9 +46,21 @@ namespace _app.Scripts.Player
             rb.AddForce(jumpForce, ForceMode.Impulse);
         }
 
+        public void FixedUpdate()
+        {
+            PlayerMovement();
+        }
+
         private void PlayerMovement()
         {
-            transform.Translate(new Vector3(3,0,0));
+            Vector3 forwardMovement = orientation.forward * verticalInput;
+            Vector3 rightMovement = orientation.right * horizontalInput;
+
+            // Combine the forward and right movement vectors
+            Vector3 movement = (forwardMovement + rightMovement).normalized * movementSpeed;
+
+            // Apply the movement to the rigidbody
+            rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
         }
     }
 }
